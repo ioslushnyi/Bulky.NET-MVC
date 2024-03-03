17d8fd6d6f9955bbf7e311bfff0e538122b177e2
Ihor <igorslushniy@gmail.com>
2024-03-03 19:47:05 +0100

    Add project files.

---
+++ BulkyWebShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkyWebShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
